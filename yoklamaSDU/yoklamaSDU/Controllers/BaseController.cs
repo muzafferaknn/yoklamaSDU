714fca58f2e6a171210efd836d9dffb582236b08
Muzaffer Akın <45818462+muzafferaknn@users.noreply.github.com>
2021-03-08 12:05:14 +0300

    Initial commit

---
+++ yoklamaSDU/yoklamaSDU/Controllers/BaseController.cs
@@ -0,0 +1,50 @@
+﻿
+using Net.SourceForge.Koogra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using yoklamaSDU.AppCode;
+using yoklamaSDU.Models.EntityFramework;
+
+namespace yoklamaSDU.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            // session timeout olduğunda login sayfasına yönlendirme
+
+            if (GetLoginUser() == null)
+            {
+                filterContext.Result = RedirectToAction("Index", "Giris");
+            }
+            else
+            {
+                string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+               
+            }
+        }
+
+        public LoginUser GetLoginUser()
+        {
+            return (LoginUser)Session["LoginUser"];
+        }
+        public string GetCellValue(IRow cells, uint column)
+        {
+            if (cells != null && cells.GetCell(column) != null)
+            {
+                object value = cells.GetCell(column).Value;
+                if (value != null)
+                {
+                    return value.ToString().Trim();
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
